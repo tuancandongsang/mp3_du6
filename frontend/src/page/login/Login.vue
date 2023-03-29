373961f59cfa05d033ad39ce4376a2b78f2f1980
tuancandongsang <tuancandongsang@gmail.com>
2023-03-29 17:31:02 +0700

    a

--- frontend/src/page/login/Login.vue
+++ frontend/src/page/login/Login.vue
@@ -6,20 +6,14 @@
       <div class="form">
         <div class="username">
           <label for="">Username:</label>
-          <input type="text" v-model="username" />
-          <div class="validate-input" v-if="isNullEmail">
-            username is required
-          </div>
+          <input type="text" v-model="username" @blur="checkName" />
           <div class="validate-input">{{ message }}</div>
         </div>
         <div class="password">
           <label for="">Password:</label>
           <input type="password" v-model="password" />
-          <div class="validate-input" v-if="isNullPassWord">
-            password is required
-          </div>
-          <div class="validate-input" >
-            {{messagePassword}}
+          <div class="validate-input">
+            {{ messagePassword }}
           </div>
         </div>
       </div>
@@ -37,11 +31,8 @@ export default {
     return {
       username: "",
       password: "",
-      isNullEmail: false,
-      isNullPassWord: false,
-      isMaxLengthEmail: false,
       message: "",
-      messagePassword : ""
+      messagePassword: "",
     };
   },
   computed: {
@@ -51,30 +42,28 @@ export default {
   },
   watch: {
     username() {
-      !this.username ? (this.isNullEmail = true) : (this.isNullEmail = false);
       this.username && this.username.length < 9
         ? (this.message = "UserName min length  is 8")
         : (this.message = "");
     },
     password() {
-      !this.password
-        ? (this.isNullPassWord = true)
-        : (this.isNullPassWord = false);
-
-        if(this.password){
-             const  regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-             const result = regularExpression.test(this.password);
-             result ? this.messagePassword = "" : this.messagePassword = "Invalid PassWord"
-        }else{
-            this.messagePassword = ""
-        }
+      if (!this.password) {
+        this.messagePassword = "Password invalid";
+      }
+      if (this.password.length < 7) {
+        this.messagePassword = "Min 6 text";
+      } else {
+        this.messagePassword = "";
+      }
     },
   },
   methods: {
     submitLogin() {
       if (this.isValidLogin && !this.messagePassword && !this.message) {
+        document.cookie=`username_zingDu6=${this.username} ; expires=${new Date('2023-03-29 14:20:00').toUTCString()}`
         this.$router.push("/");
       }
+      console.log(document.cookie);
     },
   },
 };
